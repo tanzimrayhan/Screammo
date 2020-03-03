341d65cfb7b7b20e39a3f94d3a2422d944d34326
Tanzim Ali <tanzim_rayhan@yahoo.com>
2020-03-03 12:45:56 -0500

    Added create and get screams functions

--- Screammo-functions/functions/index.js
+++ Screammo-functions/functions/index.js
@@ -1,7 +1,14 @@
 const functions = require('firebase-functions');
-const admin=require ('firebase-admin');
 
-admin.initializeApp();
+var admin = require("firebase-admin");
+
+var serviceAccount = require('./screammo-148ea-b2d72f9fa2cc.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://screammo-148ea.firebaseio.com"
+});
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -22,4 +29,23 @@ exports.getScreams=functions.https.onRequest((req,res)=>{
         .catch(err=>{
             console.log(err);
         })
-})
+});
+
+exports.createScream=functions.https.onRequest((req,res)=>{
+    const newScream={
+        body:req.body.body,
+        userHandle:req.body.userHandle,
+        createdAt:admin.firestore.Timestamp.fromDate(new Date())
+    };
+
+    admin.firestore()
+        .collection('screams')
+        .add(newScream)
+        .then(doc =>{
+            res.json({message:`document ${doc.id} created successfully!`})
+        })
+        .catch(err=>{
+            res.status(500).json({error:'something went wrong'})
+            console.error(err);
+        })
+});
