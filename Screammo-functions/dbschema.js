7b0678a3cb1158a882475ac165a049799d1fc78f
tanzimrayhan <tanzim_rayhan@yahoo.com>
2020-03-04 14:52:16 -0500

    Added schema and started auth

---
+++ Screammo-functions/dbschema.js
@@ -0,0 +1,11 @@
+let db={
+    screams:[
+        {
+            userHandle:'user', 
+            body:'this is the scream body',
+            createdAt:'2020-03-04T19:28:28.463Z',
+            likeCount:5,
+            commentCount:2
+        }
+    ]
+} 
--- Screammo-functions/functions/index.js
+++ Screammo-functions/functions/index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions');
 var admin = require("firebase-admin");
 
 var serviceAccount = require('./screammo-148ea-b2d72f9fa2cc.json');
-
+//screammo-148ea-e372653e9615
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://screammo-148ea.firebaseio.com"
@@ -11,18 +11,24 @@ admin.initializeApp({
 const express=require('express');
 const app=express();
 
-
+const firebase=require('firebase');
+firebase.initializeApp(firebaseConfig);
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Backend!");
 });
 
 app.get('/screams',(req,res)=>{
-    admin.firestore().collection('screams').get()
+    admin.firestore().collection('screams').orderBy('createdAt','desc').get()
         .then(data=>{
             let screams=[];
             data.forEach(doc=>{
-                screams.push(doc.data());
+                screams.push({
+                    screamId:doc.id,
+                    body:doc.data().body,
+                    userHandle:doc.data().userHandle,
+                    createdAt:doc.data().createdAt
+                });
             })
             return res.json(screams);
         })
@@ -37,7 +43,7 @@ app.post('/scream',((req,res)=>{
     const newScream={
         body:req.body.body,
         userHandle:req.body.userHandle,
-        createdAt:admin.firestore.Timestamp.fromDate(new Date())
+        createdAt:new Date().toISOString
     };
 
     admin.firestore()
