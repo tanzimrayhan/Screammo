ca705dd1f344609c7ed3d3959cb7948d2e53bedd
Tanzim Ali <tanzim_rayhan@yahoo.com>
2020-03-27 01:39:47 -0400

    added middleware but not working

--- Screammo-functions/functions/index.js
+++ Screammo-functions/functions/index.js
@@ -44,12 +44,45 @@ app.get('/screams', (req, res) => {
         })
 })
 
+const FBAuth=(req,res,next)=>{
 
-app.post('/scream', ((req, res) => {
+    let idToken;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+        idToken=req.headers.authorization.split('Bearer ')[1];
+
+    } else {
+        console.error('No Token Found');
+        return res.status(403).json({error:'Unauthorized'});
+    }
+
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken=>{
+            req.user=decodedToken;
+            console.log(decodedToken);
+            return db.collection('users')
+                .where('userId','==',req.user.uid)
+                .limit(1)
+                .get();
+        })
+        .then(data=>{
+            req.user.handle=data.docs[0].data().handle;
+            return next();
+        })
+        .catch(err=>{
+            console.error("Error while verifying token", err);
+            return res.status(403).json(err);
+        })
+}
+
+//Post one scream
+app.post('/scream', FBAuth, ((req, res) => {
+    if(req.body.body.trim()===''){
+        return res.status(400).json({body:'Body must not be empty'});
+    }
 
     const newScream = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         createdAt: new Date().toISOString
     };
 
@@ -75,6 +108,9 @@ const isEmpty=(string)=>{
     if(string.trim()==='') return true;
     else return false;
 }
+
+
+
 
 //Signup route
 app.post('/signup', ((req, res) => {
@@ -141,5 +177,41 @@ app.post('/signup', ((req, res) => {
 
 
 }))
+
+
+
+//Login Route
+
+app.post('/login',(req,res)=>{
+    const user={
+        email:req.body.email,
+        password:req.body.password
+    };
+
+    //Validation
+    let errors={};
+
+    if(isEmpty(user.email)) errors.email="Must not be empty";
+    if(isEmpty(user.password)) errors.password="Must not be empty";
+
+    if(Object.keys(errors).length>0){
+        return res.status(400).json(errors);
+    }
+
+    firebase.auth().signInWithEmailAndPassword(user.email,user.password)
+        .then(data=>{
+            return data.user.getIdToken();
+        })
+        .then(token=>{
+            return res.json({token});
+        })
+        .catch(err =>{
+            console.log(err);
+            if(err.code==='auth/wrong-password'){
+                return res.status(403).json({general:'Wrong Credentials, please try again'})
+            }
+            else return res.status(500).json({error:err.code});
+        })
+})
 
 exports.api = functions.https.onRequest(app);
