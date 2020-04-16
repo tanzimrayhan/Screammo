011e1ad78430b65169e4742d3ec4be5471b495e7
tanzimrayhan <tanzim_rayhan@yahoo.com>
2020-04-15 21:13:43 -0400

    Started image upload

--- Screammo-functions/functions/handlers/users.js
+++ Screammo-functions/functions/handlers/users.js
@@ -1,89 +1,119 @@
-const { db } = require('../util/admin');
-const firebase = require('firebase');
+const { db, admin } = require("../util/admin");
+const firebase = require("firebase");
 
-
-const { firebaseConfig } = require('../config');
+const { firebaseConfig } = require("../config");
 firebase.initializeApp(firebaseConfig);
 
-const { validateSignUpData, validateLoginData } = require('../util/validators');
+const { validateSignUpData, validateLoginData } = require("../util/validators");
 
-exports.signup = ((req, res) => {
-    const newUser = {
-        email: req.body.email,
-        password: req.body.password,
-        confirmPassword: req.body.confirmPassword,
-        handle: req.body.handle
-    };
+exports.signup = (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
 
+  const { valid, errors } = validateSignUpData(newUser);
 
-    const {valid,errors}= validateSignUpData(newUser);
+  if (!valid) return res.status(400).json(errors);
 
-    if(!valid) return res.status(400).json(errors);
+  //End of Validation
 
-    //End of Validation
-
-    let token, userId;
-    db.doc(`/users/${newUser.handle}`).get()
-        .then(doc => {
-            if (doc.exists) {
-                return res.status(400).json({ handle: 'this handle is already taken' })
-            } else {
-                return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
-            }
-        }).then(data => {
-            userId = data.user.uid;
-            return data.user.getIdToken();
-        })
-        .then(idtoken => {
-            token = idtoken;
-            const userCredentials = {
-                handle: newUser.handle,
-                email: newUser.email,
-                createdAt: new Date().toISOString(),
-                userId
-            };
-            return db.doc(`/users/${newUser.handle}`).set(userCredentials);
-        }).then(() => {
-            return res.status(201).json({ token });
-        })
-        .catch(err => {
-            console.error(err);
-            if (err.code === "auth/email-already-in-use") {
-                return res.status(400).json({ email: 'Email already in use' });
-            } else {
-                return res.status(500).json({ error: err.code });
-            }
-
-        })
-
-
-})
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: "this handle is already taken" });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idtoken) => {
+      token = idtoken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email already in use" });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+};
 
 exports.login = (req, res) => {
-    const user = {
-        email: req.body.email,
-        password: req.body.password
-    };
+  const user = {
+    email: req.body.email,
+    password: req.body.password,
+  };
 
-    const {valid,errors}= validateLoginData(user);
+  const { valid, errors } = validateLoginData(user);
 
-    if(!valid) return res.status(400).json(errors);
+  if (!valid) return res.status(400).json(errors);
 
+  //Validation
 
-    //Validation
-    
-    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
-        .then(data => {
-            return data.user.getIdToken();
-        })
-        .then(token => {
-            return res.json({ token });
-        })
-        .catch(err => {
-            console.log(err);
-            if (err.code === 'auth/wrong-password') {
-                return res.status(403).json({ general: 'Wrong Credentials, please try again' })
-            }
-            else return res.status(500).json({ error: err.code });
-        })
-}
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then((data) => {
+      return data.user.getIdToken();
+    })
+    .then((token) => {
+      return res.json({ token });
+    })
+    .catch((err) => {
+      console.log(err);
+      if (err.code === "auth/wrong-password") {
+        return res
+          .status(403)
+          .json({ general: "Wrong Credentials, please try again" });
+      } else return res.status(500).json({ error: err.code });
+    });
+};
+
+exports.uploadImage = (req, res) => {
+  const Busboy = require("busboy");
+  const path = require("path");
+  const os = require("os");
+  const fs = require("fs");
+
+  const busboy = new Busboy({ headers: req.headers });
+
+  let imageFileName;
+  let imageToBeUploaded = {};
+
+  busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+    console.log(fieldname);
+    console.log(filename);
+    console.log(mimetype);
+    //image.png
+    const imageExt = filename.split(".")[filename.split(".").length - 1];
+    imageFileName = `${Math.round(Math.random() * 1000000000)}.${imageExt}`;
+    const filePath = path.join(os.tmpdir(), imageFileName);
+    imageToBeUploaded = { filePath, mimetype };
+    file.pipe(fs.createWriteStream(filePath));
+  });
+
+  busboy.on('finish',() =>{
+      admin.storage().bucket()..upload( )
+  });
+};
--- Screammo-functions/functions/index.js
+++ Screammo-functions/functions/index.js
@@ -4,9 +4,7 @@ const { firebaseConfig } = require('./config');
 const FBAuth=require('./util/FBAuth')
 
 const {getAllScreams,postOneScream}= require ('./handlers/screams')
-const {signup, login,uploadImage}=require('./handlers/users');
-
-
+const {signup, login, uploadImage }=require('./handlers/users');
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Backend!");
@@ -20,6 +18,5 @@ app.post('/scream', FBAuth,postOneScream);
 app.post('/signup', signup)
 app.post('/login', login )
 app.post('/user/image', uploadImage)
-
 
 exports.api = functions.https.onRequest(app);
