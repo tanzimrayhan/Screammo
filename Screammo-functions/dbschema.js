81e40c6e5a6528e2db45cd827e6e12948a1c434e
tanzimrayhan <tanzim_rayhan@yahoo.com>
2020-04-18 03:45:59 -0400

    Added and finished image upload route, added addUserDetails route and validator

--- Screammo-functions/dbschema.js
+++ Screammo-functions/dbschema.js
@@ -1,11 +1,23 @@
-let db={
-    screams:[
-        {
-            userHandle:'user', 
-            body:'this is the scream body',
-            createdAt:'2020-03-04T19:28:28.463Z',
-            likeCount:5,
-            commentCount:2
-        }
-    ]
-} 
+let db = {
+  users: [
+    {
+      userId: "a3435jhjh5rt8jhhf8wnnhbkb3f1rd",
+      email: "user@email.com",
+      handle: "user",
+      createdAt: "2020-03-04T19:28:28.463Z",
+      imageUrl: "image/adhkaghk",
+      bio: "This is a bio",
+      website: "https://user.com",
+      location: "London, UK"
+    }
+  ],
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is the scream body",
+      createdAt: "2020-03-04T19:28:28.463Z",
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};
--- Screammo-functions/functions/index.js
+++ Screammo-functions/functions/index.js
@@ -1,22 +1,28 @@
-const functions = require('firebase-functions');
-const app = require('express')();
-const { firebaseConfig } = require('./config');
-const FBAuth = require('./util/FBAuth')
+const functions = require("firebase-functions");
+const app = require("express")();
+const { firebaseConfig } = require("./config");
+const FBAuth = require("./util/FBAuth");
 
-const { getAllScreams, postOneScream } = require('./handlers/screams')
-const { signup, login, uploadImage } = require('./handlers/users');
+const { getAllScreams, postOneScream } = require("./handlers/screams");
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails
+} = require("./handlers/users");
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Backend!");
+  response.send("Hello from Backend!");
 });
 
 //Scream routes
-app.get('/screams', getAllScreams)
-app.post('/scream', FBAuth, postOneScream);
+app.get("/screams", getAllScreams);
+app.post("/scream", FBAuth, postOneScream);
 
 // isers routes
-app.post('/signup', signup)
-app.post('/login', login)
-app.post('/user/image', FBAuth, uploadImage)
+app.post("/signup", signup);
+app.post("/login", login);
+app.post("/user/image", FBAuth, uploadImage);
+app.post("/user", FBAuth, addUserDetails);
 
 exports.api = functions.https.onRequest(app);
